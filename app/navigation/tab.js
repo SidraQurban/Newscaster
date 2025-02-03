95e44691f6f82e36ac813d82dd2913d3488442b0
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-02 21:46:39 -0800

    ../

--- app/navigation/tab.js
+++ app/navigation/tab.js
@@ -1,34 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import DiscoverScreen from "../screens/DiscoverScreen";
 import SaveScreen from "../screens/SaveScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import { responsiveHeight } from "react-native-responsive-dimensions";
+import { Ionicons } from "react-native-vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Tabs = () => {
   return (
     <Tab.Navigator
-      tabBarOptions={{
-        showLabel: false,
-        style: {
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
           position: "absolute",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: "red",
-          borderTopWidth: 0.5,
-          borderTopColor: "#ddd",
+          backgroundColor: "black",
+          height: responsiveHeight(8),
         },
       }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View>
+              <Ionicons
+                name="home"
+                size={15}
+                style={{ color: focused ? "white" : "red" }}
+              />
+              <Text style={{ color: focused ? "white" : "gray", fontSize: 10 }}>
+                Home
+              </Text>
+            </View>
+          ),
+        }}
+      />
       <Tab.Screen name="Discover" component={DiscoverScreen} />
       <Tab.Screen name="Save" component={SaveScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
-}
+};
 
 export default Tabs;
