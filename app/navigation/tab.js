bc758ada1f4eed1bb8af917cdc539b3cfb085310
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-02 23:26:47 -0800

    updated

--- app/navigation/tab.js
+++ app/navigation/tab.js
@@ -1,19 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import DiscoverScreen from "../screens/DiscoverScreen";
 import SaveScreen from "../screens/SaveScreen";
 import ProfileScreen from "../screens/ProfileScreen";
-import { responsiveFontSize, responsiveHeight, responsiveWidth } from "react-native-responsive-dimensions";
-import { Ionicons,Fontisto,FontAwesome6 } from "react-native-vector-icons";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
+import { Ionicons, Fontisto, FontAwesome5 } from "react-native-vector-icons";
 
 const Tab = createBottomTabNavigator();
+
+const CustomTabButton = (props) => (
+  <TouchableOpacity
+    {...props}
+    activeOpacity={1} // Prevents the grey ripple effect
+  />
+);
+
 const Tabs = () => {
   return (
     <Tab.Navigator
       screenOptions={{
-        headerShown:false,
+        headerShown: false,
         tabBarShowLabel: false,
         tabBarStyle: {
           position: "absolute",
@@ -26,25 +38,26 @@ const Tabs = () => {
         name="Home"
         component={HomeScreen}
         options={{
+          tabBarButton: CustomTabButton,
           tabBarIcon: ({ focused }) => (
             <View
               style={{
                 alignItems: "center",
                 justifyContent: "center",
                 top: responsiveHeight(2),
-                width:responsiveWidth(20)
+                width: responsiveWidth(20),
               }}
             >
               <Ionicons
                 name="home"
                 size={25}
-                style={{ color: focused ? "#2196f3" : "#979dac" }}
+                color={focused ? "#2196f3" : "#979dac"}
               />
               <Text
                 style={{
                   color: focused ? "#2196f3" : "#979dac",
                   fontSize: responsiveFontSize(1.7),
-                  fontWeight:"600",
+                  fontWeight: "600",
                   marginTop: responsiveHeight(0.5),
                 }}
               >
@@ -54,54 +67,63 @@ const Tabs = () => {
           ),
         }}
       />
-      <Tab.Screen name="Discover" component={DiscoverScreen}  options={{
+      <Tab.Screen
+        name="Discover"
+        component={DiscoverScreen}
+        options={{
+          tabBarButton: CustomTabButton,
           tabBarIcon: ({ focused }) => (
             <View
               style={{
                 alignItems: "center",
                 justifyContent: "center",
                 top: responsiveHeight(2),
-                width:responsiveWidth(20)
+                width: responsiveWidth(20),
               }}
             >
               <Fontisto
-                name="world-o"
+                name="world"
                 size={25}
-                style={{ color: focused ? "#2196f3" : "#979dac" }}
+                color={focused ? "#2196f3" : "#979dac"}
               />
               <Text
                 style={{
-                    color: focused ? "#2196f3" : "#979dac",
+                  color: focused ? "#2196f3" : "#979dac",
                   fontSize: responsiveFontSize(1.7),
                   marginTop: responsiveHeight(0.5),
-                  fontWeight:"600",
+                  fontWeight: "600",
                 }}
               >
-                Discover 
+                Discover
               </Text>
             </View>
           ),
-        }}/>
-      <Tab.Screen name="Save" component={SaveScreen}  options={{
+        }}
+      />
+      <Tab.Screen
+        name="Save"
+        component={SaveScreen}
+        options={{
+          tabBarButton: CustomTabButton,
           tabBarIcon: ({ focused }) => (
             <View
               style={{
                 alignItems: "center",
                 justifyContent: "center",
                 top: responsiveHeight(2),
-                width:responsiveWidth(20)
+                width: responsiveWidth(20),
               }}
             >
               <Fontisto
                 name="favorite"
                 size={25}
-                style={{ color: focused ? "#2196f3" : "#979dac" }}
+                color={focused ? "#2196f3" : "#979dac"}
               />
               <Text
                 style={{
-                    color: focused ? "#2196f3" : "#979dac",
+                  color: focused ? "#2196f3" : "#979dac",
                   fontSize: responsiveFontSize(1.7),
-                  fontWeight:"600",
+                  fontWeight: "600",
                   marginTop: responsiveHeight(0.5),
                 }}
               >
@@ -109,35 +131,41 @@ const Tabs = () => {
               </Text>
             </View>
           ),
-        }} />
-      <Tab.Screen name="Profile" component={ProfileScreen}  options={{
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarButton: CustomTabButton,
           tabBarIcon: ({ focused }) => (
             <View
               style={{
                 alignItems: "center",
                 justifyContent: "center",
                 top: responsiveHeight(2),
-                width:responsiveWidth(20)
+                width: responsiveWidth(20),
               }}
             >
-              <FontAwesome6
-                name="user"
+              <FontAwesome5
+                name="user-alt"
                 size={25}
-                style={{ color: focused ? "#2196f3" : "#979dac" }}
+                color={focused ? "#2196f3" : "#979dac"}
               />
               <Text
                 style={{
-                    color: focused ? "#2196f3" : "#979dac",
+                  color: focused ? "#2196f3" : "#979dac",
                   fontSize: responsiveFontSize(1.7),
                   marginTop: responsiveHeight(0.5),
-                  fontWeight:"600",
+                  fontWeight: "600",
                 }}
               >
                 Profile
               </Text>
             </View>
           ),
-        }} />
+        }}
+      />
     </Tab.Navigator>
   );
 };
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,90 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {MaterialCommunityIcons, Ionicons, Fontisto} from "react-native-vector-icons"
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{ padding: responsiveWidth(2) }}>
+      <SafeAreaView>
+        <View
+          style={{
+            backgroundColor: "#dee2e6",
+            height: responsiveHeight(5),
+            width: responsiveHeight(5),
+            borderRadius: responsiveHeight(2.8),
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "white",
+              height: responsiveHeight(4.5),
+              width: responsiveHeight(4.5),
+              borderRadius: responsiveHeight(2.5),
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <MaterialCommunityIcons name="menu" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            gap: responsiveWidth(1),
+            marginTop: responsiveHeight(-5),
+            marginLeft: responsiveWidth(72),
+            backgroundColor: "#dee2e6",
+            height: responsiveHeight(5),
+            width: responsiveHeight(11),
+            borderRadius: responsiveHeight(2.8),
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "white",
+              height: responsiveHeight(4.7),
+              width: responsiveHeight(4.7),
+              borderRadius: responsiveHeight(2.5),
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Ionicons name="search" size={20} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: "white",
+              height: responsiveHeight(4.7),
+              width: responsiveHeight(4.7),
+              borderRadius: responsiveHeight(2.5),
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Fontisto name="bell" size={20} color="black" />
+            {/* Red dot */}
+            <View
+              style={{
+                position: "absolute",
+                top: 9, // Adjust the position of the red dot
+                right: 9, // Adjust the position of the red dot
+                width: 6.5, // Size of the red dot
+                height: 6.5, // Size of the red dot
+                borderRadius: 3.5, // Make it round
+                backgroundColor: "red", // Red color
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
-  )
+  );
 }
 
 export default HomeScreen
