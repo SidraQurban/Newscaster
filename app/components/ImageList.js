207b44054bf360ad1392ee64d59ee00383f16e93
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-02 23:31:25 -0800

    ../

--- app/components/ImageList.js
+++ app/components/ImageList.js
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { responsiveWidth } from 'react-native-responsive-dimensions'
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 
 const ImageList = () => {
   return (
     <View style={{ padding: responsiveWidth(2) }}>
-        <View style={{}}>
-        <Text>Breaking News</Text>
+        <View style={{marginTop:responsiveHeight(3)}}>
+        <Text sty={{}}>Breaking News</Text>
         </View>
     
     </View>
