9af3d90d19a5c03eebd5d2292ed11f4d10f3a90a
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-03 17:39:40 -0800

    updated

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,93 +1,98 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import {MaterialCommunityIcons, Ionicons, Fontisto} from "react-native-vector-icons"
-import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
-import ImageList from '../components/ImageList'
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialCommunityIcons, Ionicons, Fontisto } from "react-native-vector-icons";
+import { responsiveHeight, responsiveWidth } from "react-native-responsive-dimensions";
+import ImageList from "../components/ImageList";
+
 const HomeScreen = () => {
   return (
     <View style={{ padding: responsiveWidth(2) }}>
-      <SafeAreaView>
-        <View
-          style={{
-            backgroundColor: "#dee2e6",
-            height: responsiveHeight(5),
-            width: responsiveHeight(5),
-            borderRadius: responsiveHeight(2.8),
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              backgroundColor: "white",
-              height: responsiveHeight(4.5),
-              width: responsiveHeight(4.5),
-              borderRadius: responsiveHeight(2.5),
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <MaterialCommunityIcons name="menu" size={20} color="black" />
-          </TouchableOpacity>
-        </View>
-
-        <View
-          style={{
-            flexDirection: "row",
-            gap: responsiveWidth(1),
-            marginTop: responsiveHeight(-5),
-            marginLeft: responsiveWidth(72),
-            backgroundColor: "#dee2e6",
-            height: responsiveHeight(5),
-            width: responsiveHeight(11),
-            borderRadius: responsiveHeight(2.8),
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              backgroundColor: "white",
-              height: responsiveHeight(4.7),
-              width: responsiveHeight(4.7),
-              borderRadius: responsiveHeight(2.5),
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Ionicons name="search" size={20} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "white",
-              height: responsiveHeight(4.7),
-              width: responsiveHeight(4.7),
-              borderRadius: responsiveHeight(2.5),
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Fontisto name="bell" size={20} color="black" />
-            {/* Red dot */}
+      <FlatList
+        ListHeaderComponent={(
+          <SafeAreaView>
             <View
               style={{
-                position: "absolute",
-                top: 9, // Adjust the position of the red dot
-                right: 9, // Adjust the position of the red dot
-                width: 6.5, // Size of the red dot
-                height: 6.5, // Size of the red dot
-                borderRadius: 3.5, // Make it round
-                backgroundColor: "red", // Red color
+                backgroundColor: "#dee2e6",
+                height: responsiveHeight(5),
+                width: responsiveHeight(5),
+                borderRadius: responsiveHeight(2.8),
+                justifyContent: "center",
+                alignItems: "center",
               }}
-            />
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-      {/* body */}
-     <ImageList/>
+            >
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "white",
+                  height: responsiveHeight(4.5),
+                  width: responsiveHeight(4.5),
+                  borderRadius: responsiveHeight(2.5),
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <MaterialCommunityIcons name="menu" size={20} color="black" />
+              </TouchableOpacity>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                gap: responsiveWidth(1),
+                marginTop: responsiveHeight(-5),
+                marginLeft: responsiveWidth(72),
+                backgroundColor: "#dee2e6",
+                height: responsiveHeight(5),
+                width: responsiveHeight(11),
+                borderRadius: responsiveHeight(2.8),
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "white",
+                  height: responsiveHeight(4.7),
+                  width: responsiveHeight(4.7),
+                  borderRadius: responsiveHeight(2.5),
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Ionicons name="search" size={20} color="black" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "white",
+                  height: responsiveHeight(4.7),
+                  width: responsiveHeight(4.7),
+                  borderRadius: responsiveHeight(2.5),
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Fontisto name="bell" size={20} color="black" />
+                {/* Red dot */}
+                <View
+                  style={{
+                    position: "absolute",
+                    top: 9,
+                    right: 9,
+                    width: 6.5,
+                    height: 6.5,
+                    borderRadius: 3.5,
+                    backgroundColor: "red",
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+          </SafeAreaView>
+        )}
+        ListFooterComponent={<ImageList />} 
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
-}
+};
 
-export default HomeScreen
+export default HomeScreen;
