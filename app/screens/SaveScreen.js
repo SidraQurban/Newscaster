816e76b72ba1e53d29bf1162fa967c4a0dc1c8e4
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-02-05 21:51:22 -0800

    ../

--- app/screens/SaveScreen.js
+++ app/screens/SaveScreen.js
@@ -1,17 +1,34 @@
 import { View, Text, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions'
 import { useTheme } from '../ThemeProvider'
 import { Image } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import GlobalApi from '../services/GlobalApi'
+import { useIsFocused } from '@react-navigation/native'
 
 const SaveScreen = () => {
-  const {isDarkMode} = useTheme();
-  const [bookmarkNews, setBookmarkNews] =useState([]); 
+  const { isDarkMode } = useTheme();
+  const [bookmarkNews, setBookmarkNews] = useState([]);
+  const isFocused = useIsFocused(); // This gives a boolean value (true/false)
 
-  
+  useEffect(() => {
+    if (isFocused) {
+      fetchBookmark(); // Fetch bookmarks only when the screen is focused
+    }
+  }, [isFocused]); // Re-run when the focus state changes
+
+  const fetchBookmark = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@bookmarkNews');
+      if (value !== null) {
+        setBookmarkNews(JSON.parse(value));
+      } else {
+        setBookmarkNews([]); // No saved bookmarks
+      }
+    } catch (error) {
+      console.error('Error fetching bookmarks: ', error); // Handle any error that occurs
+    }
+  };
 
   return (
     <View
@@ -39,36 +56,37 @@ const SaveScreen = () => {
           Saved
         </Text>
       </View>
-      {/* body */}
-    <FlatList
-     data={bookmarkNews}
-     renderItem={({item}) => (
-      <View style={{ flexDirection: "row" }}>
-      <Image
-        source={{uri: item.urlToImage}}
-        style={{
-          height: responsiveHeight(25),
-          width: responsiveWidth(50),
-          resizeMode: "contain",
-        }}
+
+      {/* FlatList to render saved bookmarks */}
+      <FlatList
+        data={bookmarkNews}
+        renderItem={({ item }) => (
+          <View style={{ flexDirection: "row", marginTop: responsiveHeight(2) }}>
+            <Image
+              source={{ uri: item.urlToImage }}
+              style={{
+                height: responsiveHeight(25),
+                width: responsiveWidth(50),
+                resizeMode: "contain",
+              }}
+            />
+            <Text
+              style={{
+                marginTop: responsiveHeight(6),
+                color: isDarkMode ? "white" : "black",
+                fontSize: responsiveFontSize(2),
+                fontWeight: "500",
+                marginLeft: responsiveWidth(2),
+              }}
+            >
+              {item.title}
+            </Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.title}
       />
-      <Text
-        style={{
-          marginTop: responsiveHeight(6),
-          color: isDarkMode ? "white" : "black",
-          fontSize:responsiveFontSize(2),
-          fontWeight:"500"
-        }}
-      >
-       {item.title}
-      </Text>
-    </View>
-     )}
-    
-    />
-     
     </View>
   );
-}
+};
 
-export default SaveScreen
+export default SaveScreen;
