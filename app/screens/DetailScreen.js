5e4165c27d7082c1b340dfd5b564c142ef9d0ef8
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-01 20:52:56 -0700

    ./

--- app/screens/DetailScreen.js
+++ app/screens/DetailScreen.js
@@ -19,7 +19,7 @@ useEffect(() => {
   console.log(news);
 }, []);
 
-// Check if the news is bookmarked when the component mounts or when the news change
+// Check if the news is bookmarked when the component mounts or when the news 
 useFocusEffect(
   React.useCallback(() => {
     if (news) {
