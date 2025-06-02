6cf6853b2613ac5df9be0089a73686ff5b013629
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-01 21:18:25 -0700

    updated

--- app/screens/SaveScreen.js
+++ app/screens/SaveScreen.js
@@ -1,24 +1,69 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
+import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 import { useTheme } from '../ThemeProvider';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
+
 
 const SaveScreen = () => {
   const { isDarkMode, toggleTheme } = useTheme();
+  const navigation = useNavigation();
   
   return (
-    <SafeAreaView>
-      <View
-        style={{
+    <SafeAreaView  style={{
           padding: responsiveWidth(3),
-          //  backgroundColor: isDarkMode ? "#212529" : "#f8f9fa",
-        }}
-      >
-        <View>
- <Text>SaveScreen</Text>
-        </View>
-       
+           backgroundColor: isDarkMode ? "#212529" : "#f8f9fa",
+        }}>
+      <View>
+         {/* Header */}
+            <View
+              style={{
+                backgroundColor: isDarkMode ? "#343a40" : "#dee2e6",
+                height: responsiveHeight(5),
+                width: responsiveHeight(5),
+                borderRadius: responsiveHeight(2.8),
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                style={{
+                  backgroundColor: isDarkMode ? "#212529" : "white",
+                  height: responsiveHeight(4.5),
+                  width: responsiveHeight(4.5),
+                  borderRadius: responsiveHeight(2.5),
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Ionicons
+                  name="chevron-back"
+                  size={20}
+                  color={isDarkMode ? "white" : "black"}
+                />
+              </TouchableOpacity>
+            </View>
+             {/* Saved Section */}
+            <View
+              style={{
+                padding: responsiveWidth(1),
+                marginTop: responsiveHeight(2),
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: responsiveFontSize(2.7),
+                  fontWeight: "bold",
+                  color: isDarkMode ? "white" : "black",
+                }}
+              >
+                Saved
+              </Text>
+              </View>
       </View>
     </SafeAreaView>
   );
