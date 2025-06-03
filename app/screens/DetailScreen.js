10181bfc77ea373c112ba94b10ab87de443589b6
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-02 21:21:28 -0700

    ./

--- app/screens/DetailScreen.js
+++ app/screens/DetailScreen.js
@@ -80,7 +80,7 @@ const renderBookmark = async (newsTitle) => {
      <TouchableOpacity
        onPress={() => {
         setFav(fav === news.title ? null : news.title);
-    saveBookmark(news.title);
+        saveBookmark(news.title);
   }}
 > 
  <Text>Save</Text>
