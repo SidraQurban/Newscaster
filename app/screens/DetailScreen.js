fd0280847a2ccbb90bd8665b8b742fd6584e5c10
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-07 00:15:08 -0700

    ./

--- app/screens/DetailScreen.js
+++ app/screens/DetailScreen.js
@@ -82,7 +82,7 @@ const renderBookmark = async (newsTitle) => {
            saveBookmark(news.title);
          }}
        >
-         <Text>Save</Text>
+       <Text>Save</Text>
      </TouchableOpacity>;
   return (
     <ScrollView
