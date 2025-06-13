8470b788ccb5edee6063b8902798b02f699341ff
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-12 23:40:50 -0700

    ./

--- app/screens/DetailScreen.js
+++ app/screens/DetailScreen.js
@@ -94,6 +94,8 @@ const renderBookmark = async (newsTitle) => {
       }}
     >
     <View
+    
+
         style={{
           padding: responsiveWidth(3),
           backgroundColor: isDarkMode ? "#212529" : "#f8f9fa",
