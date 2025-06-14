dcbf80d8a44314fc84c1775f25840602c2a7f13b
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-14 02:36:49 -0700

    updated

--- app/screens/DiscoverScreen.js
+++ app/screens/DiscoverScreen.js
@@ -35,7 +35,6 @@ const DiscoverScreen = () => {
     setFilteredNewsList(result.articles); 
     setLoading(false);
   };
-
 const handleSearch = (text) => {
     setText(text);
     if (text === "") {
