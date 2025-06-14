abd9b74b3a4893d624d31b97955574089fb32c43
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-14 02:36:01 -0700

    ./

--- app/screens/SaveScreen.js
+++ app/screens/SaveScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react'
+import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 import { useTheme } from '../ThemeProvider';
