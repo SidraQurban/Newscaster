0abef587c49e49b5dd19e2e600369c0bb525c8cf
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-27 22:09:18 -0700

    ./

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -16,7 +16,7 @@ const {isDarkMode} = useTheme();
         padding: responsiveWidth(3),
         backgroundColor: isDarkMode ? "#212529" : "#f8f9fa",
       }}
-    >    {/* Header */}
+    >   {/* Header */}
       <FlatList
         ListHeaderComponent={
           <SafeAreaView>
