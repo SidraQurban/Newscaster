4177bed445461c781fa671830a25140c7ceba537
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-08-31 07:20:16 -0700

    ./

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -5,7 +5,7 @@ import DetailScreen from "../screens/DetailScreen";
 import tab from "../navigation/tab";
 import WelcomeScreen from "../screens/WelcomeScreen";
 
-const 
+cons;
 const Stack = createNativeStackNavigator();
 const AppNavigation = () => {
   return (
