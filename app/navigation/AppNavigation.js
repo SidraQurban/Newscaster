c03de3e74f6afe568e23d25cdb9e99f8ecfcbe8c
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-08-31 07:17:18 -0700

    updated

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 import DetailScreen from "../screens/DetailScreen";
-import tab from '../navigation/tab'
-import WelcomeScreen from '../screens/WelcomeScreen';
+import tab from "../navigation/tab";
+import WelcomeScreen from "../screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
 const AppNavigation = () => {
   return (
-   <NavigationContainer>
-    <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName='Welcome'>
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="tab" component={tab} />
         <Stack.Screen name="Detail" component={DetailScreen} />
-    </Stack.Navigator>
-   </NavigationContainer>
-  )
-  <
-}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 export default AppNavigation;
